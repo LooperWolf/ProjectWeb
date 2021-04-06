b2b525828d30c9d1fe630b677c50eedd9c215b35
btriya <briantriya@gmail.com>
2021-04-06 22:35:58 +0700

    potato

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.potato = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+        this.array = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5],[1, 2, 3, 4, 5]]
         this.backuppotato = []
         this.state = {
             Uvalue: '',
@@ -39,19 +38,18 @@ class Home extends Component {
     }
     render() {
         return (
-            <div className="App">
+            <div className="App" >
                 <div style={{ backgroundColor: '#282c34', height: '100vh', padding: '30vh', paddingTop: 10, paddingBottom: 10 }}>
                         <div style={{ color: 'white' }}>You have a preset of Ten Pagers!</div>
                         <div style={{ color: 'white' }}>There are no Features to Add more</div>
                         <div style={{ color: 'white' }}>So don't be greedy</div>
-                    {this.potato.map((ara,mtx) => {
-                        return (<div style={{ flexDirection: 'row', display: 'flex', justifyContent: 'center' }}>
+                    {this.array.map((ara,mtx) => {
+                        return (<div key={mtx} style={{ flexDirection: 'row', display: 'flex', justifyContent: 'center' }}>
                             {ara.map((data, itx) => {
-                                return <div onClick={() => this.test(itx)} style={{ justifyContent:'center',alignItems:'center',color:'white',width: 150, height: 150, backgroundColor: '#5e677a', borderRadius: 10, boxShadow: (-5 + itx * 2.5).toString() + 'px 10px 1px #222222', margin: 20 }}> {(5*mtx)+itx+1}</div>
+                                return <div key={((5*mtx)+itx+1).toString} onClick={() => this.test(itx)} style={{ justifyContent:'center',alignItems:'center',color:'white',width: 150, height: 150, backgroundColor: '#5e677a', borderRadius: 10, boxShadow: (-5 + itx * 2.5).toString() + 'px 10px 1px #222222', margin: 20 }}> {(5*mtx)+itx+1}</div>
                             })}
                         </div>)
                     })}
-
                 </div>
             </div>
         );
